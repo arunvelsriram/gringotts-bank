5de9e05db4190cbfcb8eaa9aff4ae10f5f487511
Arunvel Sriram <arunvelsriram@gmail.com>
2025-02-25 14:56:42 +0530

    Introduce fiber and add health endpoint in all services

--- cmd/customer/main.go
+++ cmd/customer/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"health": "ok"})
+	})
+
+	if err := app.Listen(":8081"); err != nil {
+		panic(err)
+	}
 }
--- cmd/recommendations/main.go
+++ cmd/recommendations/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"health": "ok"})
+	})
+
+	if err := app.Listen(":8082"); err != nil {
+		panic(err)
+	}
 }
